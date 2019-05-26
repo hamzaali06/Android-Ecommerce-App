43dc4bb202b5825a0d229f9843df59dae65f01e0
hamzaali06 <hamza.006ali@gmail.com>
2019-05-26 15:36:49 +0500

    Add New Product

--- app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
@@ -2,11 +2,18 @@ package com.example.ecommerce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.rey.material.widget.Button;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String CategoryName;
+    private Button AddNewProductButton;
+    private ImageView InputProductImage;
+    private EditText InputProductName,InputProductDescription,InputProductPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,10 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
         CategoryName = getIntent().getExtras().get("category").toString();
 
+        AddNewProductButton = (Button) findViewById(R.id.add_new_product);
+        InputProductImage = (ImageView) findViewById(R.id.select_product_image);
+        InputProductName = (EditText) findViewById(R.id.product_name);
+        InputProductDescription = (EditText) findViewById(R.id.product_description);
+        InputProductPrice = (EditText) findViewById(R.id.product_price);
     }
 }
